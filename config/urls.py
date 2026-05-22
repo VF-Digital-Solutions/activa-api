1388741cf6c355c2fc3dd94ad68369fc39fe1167
Javier Vallejo <javistainer98@gmail.com>
2026-05-22 15:55:47 -0400

    feat(identity): add JWT auth endpoints, serializers and urls

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from api.v1.router import urlpatterns as v1_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/", include("api.v1.router")),
+    path("api/v1/", include(v1_urls)),
 ]
